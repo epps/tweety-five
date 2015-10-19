00d136615ce9c531f8d0fb5230e6168b4d0d5a81
Erik Eppel <eceppel@gmail.com>
2015-10-18 17:57:00 -0700

    [Feat] Implements API call and data processing funciontality

--- server/server.js
+++ server/server.js
@@ -1,28 +1,51 @@
 // Load environmental variables
 require('dotenv').load();
+
 // Require (and instantiate) express, http, socket.io and twit
-var express = require('express');
+var express = require( 'express' );
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var Twit = require('Twit');
+var http = require( 'http' ).Server( app );
+var io = require( 'socket.io' )( http );
+var Twit = require( 'Twit' );
 var T = new Twit({
   consumer_key: process.env.consumerKey,
   consumer_secret: process.env.consumerSecret,
   access_token: process.env.accessToken,
   access_token_secret: process.env.accessTokenSecret
 });
+
 // Serve static assets from /client
-app.use(express.static(__dirname + '/../client'));
-// Set .on method for socket connection to io
-io.on('connection', function(socket) {
-  console.log('Client connected');
+app.use( express.static( __dirname + '/../client' ) );
+
+// Listen for socket connection
+io.on( 'connection', function( socket ) {
+  console.log( 'Client connected' );
+
+  // Listen for search event
+  socket.on( 'search user', searchUserTimeline );
+
 });
 
-// Create utility function for handling GET requests to Twitter API
+// Utility functions for searching timelines and processing the data returned  
+function searchUserTimeline( screenName ) {
+  // fetch user timeline from Twitter REST API
+  T.get( 'statuses/user_timeline', { screen_name: screenName, count: 25 }, getTimeLineCB );
+}
 
+function getTimeLineCB( error, data, response ) {
+  if ( error ) {
+    console.log('Error: ', error );
+    io.emit( 'search error', error.message );
+  } else {
+    var tweets = [];
+    data.forEach( function( tweet ) {
+      tweets.push( { text: tweet.text, timeStamp: (new Date(tweet.created_at)) } );
+    });
+    io.emit( 'search results', tweets );
+  }
+}
 
 // Set the port for http server 
-http.listen(8000, function() {
-  console.log('Server listening on 8000');
-})
+http.listen( 8000, function() {
+  console.log( 'Server listening on 8000' );
+});
